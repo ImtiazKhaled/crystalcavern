113f996b7afad8c79c088bc47dacda58e90cb623
imtiaz <imtiazkhaled07@gmail.com>
2021-04-09 07:24:12 -0500

    Added enemy pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Pathfinding;
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
     public int agroDistance = 100;
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    Transform enemyPosition;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        enemyPosition = GetComponent<Transform>();
+
+        InvokeRepeating("UpdatePath", 0f, 1f);
+    }
+    void Update()
+    {
+        if(path == null)
+        {
+            return;
+        }
+
+        if(currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        if ( distance < nextWaypointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            enemyPosition.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            enemyPosition.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    # region Enemy Pathfinding 
+    void UpdatePath()
+    {
+        if(seeker.IsDone()){
+            seeker.StartPath(rb.position, target.position, OnPathFound);
+        }
+    }
+    void OnPathFound(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+    #endregion
+
+    #region Enemy Health
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -21,14 +98,5 @@ public class Enemy : MonoBehaviour
         // Play die animation here
         Destroy(gameObject);
     }
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    #endregion
 }
