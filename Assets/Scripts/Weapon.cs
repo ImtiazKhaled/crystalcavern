40813896890af5965d7da1c328c1035358e68c26
imtiaz <imtiazkhaled07@gmail.com>
2021-04-09 15:52:14 -0500

    Added player and weapon sprites into game

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject projectile;
+    public GameObject projectileObj;
     public GameObject player;
     public Transform firepoint;
+    public Animator animator;
     public float projectileForce = 20f;
 
     void Update()
@@ -19,12 +20,15 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        // Spawn a projectile from the firepoint and add a force to it
-        GameObject weapon = Instantiate(projectile, firepoint.position, firepoint.rotation);
-        Rigidbody2D rb = weapon.GetComponent<Rigidbody2D>();
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("BowShooting")) {
+            animator.SetTrigger("IsShooting");
+            // Spawn a projectile from the firepoint and add a force to it
+            GameObject projectile = Instantiate(projectileObj, firepoint.position, firepoint.rotation);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
 
-        // Ignores collisions with player and gun
-        Physics2D.IgnoreCollision(weapon.GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
-        rb.AddForce(firepoint.right * projectileForce, ForceMode2D.Impulse);
+            // Ignores collisions with player and gun
+            Physics2D.IgnoreCollision(projectile.GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
+            rb.AddForce(firepoint.right * projectileForce, ForceMode2D.Impulse);
+        }
     }
 }
